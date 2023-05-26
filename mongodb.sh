5902810fd21d13c753a8ab742ca4b6c57e04f93e
Smith <smith@hdudhhd.com>
2023-05-26 09:51:01 -0500

    Second Commit 5

--- mongodb.sh
+++ mongodb.sh
@@ -5,5 +5,5 @@ echo -e "\e[33mInstall MongoDB Server\e[0m"
 yum install mongodb-org -y
 
 echo -e "\e[31mStart Mongodb Server\e[0m"
-system enable mongodb
-system restart mongod
+system enable mongod
+system restart mongo
