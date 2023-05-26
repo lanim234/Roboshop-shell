c326bec0f0f23bd020d1968a5004cba1a7c13345
Smith <smith@hdudhhd.com>
2023-05-26 11:45:51 -0500

    Second Commit 6

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,9 @@
 echo -e "\e[31mCopy MongoDb Repo File\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstall MongoDB Server\e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[31mStart Mongodb Server\e[0m"
-system enable mongod
-system restart mongod
+system enable mongod &>>/tmp/roboshop.log
+system restart mongod &>>/tmp/roboshop.log
