b306cf44548fc49da92049a14d0e3ba4363cceef
Smith <smith@hdudhhd.com>
2023-05-27 07:00:55 -0500

    commit 9

---
+++ redis.sh
@@ -0,0 +1,15 @@
+echo -e "\e[34mInstalling Repo\e[0m"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
+
+echo -e "\e[34m Enable Redis 6.2\e[0m"
+yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
+
+echo -e "\e[34m Installing Redis\e[[0m"
+yum install redis -y &>>/tmp/roboshop.log
+
+echo -e "\e[34m Installing Redis\e[[0m"
+sed -i 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
+
+echo -e "\e[34m Start Redis\e[0m"
+systemctl enable redis &>>/tmp/roboshop.log
+systemctl start redis  &>>/tmp/roboshop.log
