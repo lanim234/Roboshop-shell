2a37aa41ead3e8b3571254877939d07ca9eaf370
Smith <smith@hdudhhd.com>
2023-05-27 07:27:53 -0500

    commit 9

--- redis.sh
+++ redis.sh
@@ -7,7 +7,7 @@ yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 echo -e "\e[34m Installing Redis\e[[0m"
 yum install redis -y &>>/tmp/roboshop.log
 
-echo -e "\e[34m Installing Redis\e[[0m"
+echo -e "\e[34m Update Redis lIsten address\e[[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 
 echo -e "\e[34m Start Redis\e[0m"
