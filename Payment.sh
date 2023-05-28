fe2ec65e691a39b65b3533af5be48aed1a3d5826
Smith <smith@hdudhhd.com>
2023-05-28 08:39:03 -0500

    commit 9

--- Payment.sh
+++ Payment.sh
@@ -1,29 +1,29 @@
 echo -e "\e[35m Installing Python\e[0m"
-yum install python36 gcc python3-devel -y
+yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
 
 echo -e "\e[35m Creating App user\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[35m Create a drirectory\e[0m"
 mkdir /app
 
 echo -e "\e[35m Downloading Payment Artifacts\e[0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
 
 
 echo -e "\e[35m Extracting Artifacts\e[0m"
 cd /app
-unzip /tmp/payment.zip
+unzip /tmp/payment.zip &>>/tmp/roboshop.log
 
 echo -e "\e[35m Installing Repos\e[0m"
 cd /app
-pip3.6 install -r requirements.txt
+pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
 
 
 echo -e "\e[35m Setting Up SystemD\e[0m"
-cp /home/centos/Roboshop-shell/Payment.service /etc/systemd/system/payment.service
+cp /home/centos/Roboshop-shell/Payment.service /etc/systemd/system/payment.service &>>/tmp/roboshop.log
 
 echo -e "\e[35m Restarting payment\e[0m"
-systemctl daemon-reload
-systemctl enable payment
-systemctl start payment
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable payment &>>/tmp/roboshop.log
+systemctl start payment &>>/tmp/roboshop.log
