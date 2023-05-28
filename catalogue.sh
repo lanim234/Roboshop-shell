ad0d11c3d1003e7f6a8a5daf90ca0cf936538749
Smith <smith@hdudhhd.com>
2023-05-28 16:52:10 -0500

    commit 9

--- catalogue.sh
+++ catalogue.sh
@@ -18,24 +18,24 @@ echo -e "${color}Create Application Directory${nocolor}"
 rm -rf ${app_path} &>>${log_file}
 mkdir ${app_path}
 
-echo -e "${color}Download $component Artifacts${nocolor}"
-curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
+echo -e "${color}Download ${component} Artifacts${nocolor}"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
 cd ${app_path}
 
 echo -e "${color}Extract Application Content${nocolor}"
-unzip /tmp/$component.zip &>>${log_file}
+unzip /tmp/${component}.zip &>>${log_file}
 cd ${app_path}
 
 echo -e "${color} Install NodeJS Dependencies${nocolor}"
 npm install &>>${log_file}
 
 echo -e "${color}Set up SystemD Service${nocolor}"
-cp  /home/centos/Roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
+cp  /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
-echo -e "${color}Start $component Service${nocolor}"
+echo -e "${color}Start ${component} Service${nocolor}"
 systemctl daemon-reload &>>${log_file}
-systemctl enable $component &>>${log_file}
-systemctl start $component &>>${}log_file}
+systemctl enable ${component} &>>${log_file}
+systemctl start ${component} &>>${}log_file}
 
 echo -e "${color}Copy MongoDB Repo File${nocolor}"
 cp  /home/centos/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
@@ -44,4 +44,4 @@ echo -e "${color}Install MongodDB Client${nocolor}"
 yum install mongodb-org-shell -y &>>${log_file}
 
 echo -e "${color} Load Schema${nocolor}"
-mongo --host mongodb-dev.devopsb73.shop <${app_path}/schema/$component.js &>>${log_file}
+mongo --host mongodb-dev.devopsb73.shop <${app_path}/schema/${component}.js &>>${log_file}
