71675ae9cdb51c58d7b9669cd043715fc4564441
Smith <smith@hdudhhd.com>
2023-05-28 09:43:46 -0500

    commit 9

--- Dispatch.sh
+++ Dispatch.sh
@@ -1,30 +1,30 @@
 echo -e "\e[32m Downlaoding GoLang\e[0m"
-yum install golang -y
+yum install golang -y &>>/tmp/roboshop.log
 
 echo -e "\e[32m Creating App user\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[32m Creating App Directory & Downlaoding Artifacts\e[0m"
 mkdir /app
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>>/tmp/roboshop.log
 
 echo -e "\e[32m  Extracting Artifacts\e[0m"
 cd /app
-unzip /tmp/dispatch.zip
+unzip /tmp/dispatch.zip &>>/tmp/roboshop.log
 
 echo -e "\e[32m Downloading Dependencies\e[0m"
 cd /app
-go mod init dispatch
-go get
-go build
+go mod init dispatch &>>/tmp/roboshop.log
+go get &>>/tmp/roboshop.log
+go build &>>/tmp/roboshop.log
 
 echo -e "\e[36m Setting up SystemD\e[0m"
-cd /home/centos/Roboshop-shell/Dispatch.service /etc/systemd/system/dispatch.service
+cd /home/centos/Roboshop-shell/Dispatch.service /etc/systemd/system/Dispatch.service &>>/tmp/roboshop.log
 
 echo -e "\e[32m Restarting Dispatch\e[0m"
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl start dispatch
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable dispatch &>>/tmp/roboshop.log
+systemctl start dispatch &>>/tmp/roboshop.log
 
 
 
