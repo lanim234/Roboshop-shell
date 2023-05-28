95393c3919d11e3d7458b78002aa2534217d069c
Smith <smith@hdudhhd.com>
2023-05-28 18:57:36 -0500

    commit 9

--- cart.sh
+++ cart.sh
@@ -1,36 +1,4 @@
 source common.sh
 component=cart
 
-
-echo -e "${color} Downloading NodeJs Repos${nocolor}"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-
-echo -e "${color} Downloading NodeJs${nocolor}"
-yum install nodejs -y &>>${log_file}
-
-echo -e "${color} Adding User${nocolor}"
-useradd roboshop &>>${log_file}
-
-echo -e "${color} Creating App Directory${nocolor}"
-rm -rf ${app_path} &>>${log_file}
-mkdir ${app_path}
-
-echo -e "${color} Downloading $component Artifacts${nocolor}"
-curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
-cd ${app_path}
-
-echo -e "${color} Unzipping Artifacts${nocolor}"
-unzip /tmp/$component.zip &>>${log_file}
-cd ${app_path}
-
-echo -e "${color} Downloading Depencies${nocolor}"
-npm install &>>${log_file}
-
-echo -e "${color} Setting up systemD${nocolor}"
-cp /root/Roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
-
-
-echo -e "${color} start $component${nocolor}"
-systemctl daemon-reload &>>${log_file}
-systemctl enable $component &>>${log_file}
-systemctl restart $component &>>${log_file}
+nodejs
--- common.sh
+++ common.sh
@@ -2,3 +2,39 @@ color="\e[33m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
+
+
+nodejs() {
+echo -e "${color}Download Repo${nocolor}"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+echo -e "${color}Installing NodeJs${nocolor}"
+yum install nodejs -y &>>${log_file}
+
+echo -e "${color}Adding User${nocolor}"
+useradd roboshop &>>${log_file}
+
+echo -e "${color}Create Application Directory${nocolor}"
+rm -rf ${app_path} &>>${log_file}
+mkdir ${app_path}
+
+echo -e "${color}Download ${component} Artifacts${nocolor}"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+cd ${app_path}
+
+echo -e "${color}Extract Application Content${nocolor}"
+unzip /tmp/${component}.zip &>>${log_file}
+cd ${app_path}
+
+echo -e "${color} Install NodeJS Dependencies${nocolor}"
+npm install &>>${log_file}
+
+echo -e "${color}Set up SystemD Service${nocolor}"
+cp  /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+
+echo -e "${color}Start ${component} Service${nocolor}"
+systemctl daemon-reload &>>${log_file}
+systemctl enable ${component} &>>${log_file}
+systemctl start ${component} &>>${log_file}
+
+}
