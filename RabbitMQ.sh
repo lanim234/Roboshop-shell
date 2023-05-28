b513b0fba2bc8f0a41119a35040ee5ad6000eaea
Smith <smith@hdudhhd.com>
2023-05-28 07:39:10 -0500

    commit 9

---
+++ RabbitMQ.sh
@@ -0,0 +1,16 @@
+echo -e "\e[36m Configuring Erlang Repo\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
+
+echo -e "\e[33m Configuring Yum Repos for RabbitMQ\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
+
+echo -e "\e[32m Installing RabbitMQ\e[0m"
+yum install rabbitmq-server -y &>>/tmp/roboshop.log
+
+echo -e "\e[36m start RabbitMQ\e[0m"
+systemctl enable rabbitmq-server &>>/tmp/roboshop.log
+systemctl start rabbitmq-server &>>/tmp/roboshop.log
+
+echo -e "\e[35m Create User and permission\e[0m"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
