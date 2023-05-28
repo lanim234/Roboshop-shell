649cf1cb6e92b40d6646246221fc4adc91a910bc
Smith <smith@hdudhhd.com>
2023-05-28 16:04:48 -0500

    commit 9

--- cart.sh
+++ cart.sh
@@ -1,32 +1,41 @@
-echo -e "\e[35m Downloading NodeJs Repos\e[0m"
+component=cart
+color="\e[35m"
+
+
+
+
+
+
+
+echo -e "${color} Downloading NodeJs Repos\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
-echo -e "\e[35m Downloading NodeJs\e[0m"
+echo -e "${color} Downloading NodeJs\e[0m"
 yum install nodejs -y &>>/tmp/roboshop.log
 
-echo -e "\e[35m Adding User\e[0m"
+echo -e "${color} Adding User\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[35m Creating App Directory\e[0m"
+echo -e "${color} Creating App Directory\e[0m"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
-echo -e "\e[35m Downloading Cart Artifacts\e[0m"
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
+echo -e "${color} Downloading $component Artifacts\e[0m"
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -e "\e[35m Unzipping Artifacts\e[0m"
-unzip /tmp/cart.zip &>>/tmp/roboshop.log
+echo -e "${color} Unzipping Artifacts\e[0m"
+unzip /tmp/$component.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -e "\e[35m Downloading Depencies\e[0m"
+echo -e "${color} Downloading Depencies\e[0m"
 npm install &>>/tmp/roboshop.log
 
-echo -e "\e[35m Setting up systemD\e[0m"
-cp /root/Roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
+echo -e "${color} Setting up systemD\e[0m"
+cp /root/Roboshop-shell/$component.service /etc/systemd/system/$component.service &>>/tmp/roboshop.log
 
 
-echo -e "\e[35m start Cart\e[0m"
+echo -e "${color} start $component\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable cart &>>/tmp/roboshop.log
-systemctl restart cart &>>/tmp/roboshop.log
+systemctl enable $component &>>/tmp/roboshop.log
+systemctl restart $component &>>/tmp/roboshop.log
