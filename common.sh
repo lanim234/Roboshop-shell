e0a986c3b1fdc5b46dc8da753bd581e9d4a6400d
Smith <smith@hdudhhd.com>
2023-05-28 19:34:05 -0500

    commit 9

--- common.sh
+++ common.sh
@@ -53,3 +53,43 @@ echo -e "${color} Load Schema${nocolor}"
 mongo --host mongodb-dev.devopsb73.shop <${app_path}/schema/${component}.js &>>${log_file}
 
 }
+
+
+maven() {
+  echo -e "${color} Installing Maven${nocolor}"
+  yum install maven -y &>>${log_file}
+
+  echo -e "${color} Adding user${nocolor}"
+  useradd roboshop &>>${log_file}
+
+  echo -e "${color} creating App directory${nocolor}"
+  rm -rf ${app_path} &>>${log_file}
+  mkdir ${app_path}
+
+  echo -e "${color} Downloading ${component} Artifacts${nocolor}"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+
+
+  echo -e "${color} Unzipping file${nocolor}"
+  cd ${app_path}
+  unzip /tmp/${component}.zip &>>${log_file}
+
+  echo -e "${color} Download Dependencies${nocolor}"
+  cd ${app_path}
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+
+  echo -e "${color} Setup SystemD file${nocolor}"
+  cp /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+
+  echo -e "${color} Installing MySQL Client${nocolor}"
+  yum install mysql -y &>>${log_file}
+
+  echo -e "${color} Load Schema${nocolor}"
+  mysql -h mysql-dev.devopsb73.shop -uroot -pRoboShop@1 < /schema/${component}.sql &>>${log_file}
+
+  echo -e "${color} Start ${component}${nocolor}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable ${component} &>>${log_file}
+  systemctl restart ${component} &>>${log_file
+}
--- shipping.sh
+++ shipping.sh
@@ -1,36 +1,4 @@
-echo -e "\e[36m Installing Maven\e[0m"
-yum install maven -y &>>/tmp/roboshop.log
+source common.sh
+component=component
 
-echo -e "\e[36m Adding user\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-
-echo -e "\e[36m creating App directory\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app
-
-echo -e "\e[36m Downloading Shipping Artifacts\e[0m"
-curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
-
-
-echo -e "\e[36m Unzipping file\e[0m"
-cd /app
-unzip /tmp/shipping.zip &>>/tmp/roboshop.log
-
-echo -e "\e[36m Download Dependencies\e[0m"
-cd /app
-mvn clean package &>>/tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
-
-echo -e "\e[36m Setup SystemD file\e[0m"
-cp /home/centos/Roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
-
-echo -e "\e[36m Installing MySQL Client\e[0m"
-yum install mysql -y &>>/tmp/roboshop.log
-
-echo -e "\e[36m Load Schema\e[0m"
-mysql -h mysql-dev.devopsb73.shop -uroot -pRoboShop@1 < /schema/shipping.sql &>>/tmp/roboshop.log
-
-echo -e "\e[36m Start Shipping\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable shipping &>>/tmp/roboshop.log
-systemctl restart shipping &>>/tmp/roboshop.log
+maven
