af3dfbb128cee10be23bf427bf1e1dfe038cf973
Smith <smith@hdudhhd.com>
2023-05-29 13:12:49 -0500

    commit 9

--- common.sh
+++ common.sh
@@ -1,9 +1,47 @@
-color="\e[33m"
+color="\e[36m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
 
+app_presetup() {
+  echo -e "${color}Adding User${nocolor}"
+  useradd roboshop &>>${log_file}
+
+rm -rf ${app_path} &>>${log_file}
+mkdir ${app_path}
+
+echo -e "${color} Downloading ${component} Artifacts${nocolor}"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+
+echo -e "${color}Extract Application Content${nocolor}"
+cd ${app_path}
+unzip /tmp/${component}.zip &>>${log_file}
+
+}
+
+
+systemD_setup() {
+  echo -e "${color}Set up SystemD Service${nocolor}"
+  cp  /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+
+  echo -e "${color}Start ${component} Service${nocolor}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable ${component} &>>${log_file}
+  systemctl start ${component} &>>${log_file}
+
+}
+
+
+mysql_schema_setup() {
+echo -e "${color} Installing MySQL Client${nocolor}"
+  yum install mysql -y &>>${log_file}
+
+  echo -e "${color} Load Schema${nocolor}"
+  mysql -h mysql-dev.devopsb73.shop -uroot -pRoboShop@1 < /schema/${component}.sql &>>${log_file}
+
+
+}
 nodejs() {
 echo -e "${color}Download Repo${nocolor}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
@@ -11,34 +49,14 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 echo -e "${color}Installing NodeJs${nocolor}"
 yum install nodejs -y &>>${log_file}
 
-echo -e "${color}Adding User${nocolor}"
-useradd roboshop &>>${log_file}
+app_presetup
 
-echo -e "${color}Create Application Directory${nocolor}"
-rm -rf ${app_path} &>>${log_file}
-mkdir ${app_path}
-
-echo -e "${color}Download ${component} Artifacts${nocolor}"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-cd ${app_path}
-
-echo -e "${color}Extract Application Content${nocolor}"
-unzip /tmp/${component}.zip &>>${log_file}
-cd ${app_path}
 
 echo -e "${color} Install NodeJS Dependencies${nocolor}"
 npm install &>>${log_file}
 
-echo -e "${color}Set up SystemD Service${nocolor}"
-cp  /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
-
-echo -e "${color}Start ${component} Service${nocolor}"
-systemctl daemon-reload &>>${log_file}
-systemctl enable ${component} &>>${log_file}
-systemctl start ${component} &>>${log_file}
-
+systemD_setup
 }
-
 
 
 mongo_schema_setup() {
@@ -59,37 +77,13 @@ maven() {
   echo -e "${color} Installing Maven${nocolor}"
   yum install maven -y &>>${log_file}
 
-  echo -e "${color} Adding user${nocolor}"
-  useradd roboshop &>>${log_file}
-
-  echo -e "${color} creating App directory${nocolor}"
-  rm -rf ${app_path} &>>${log_file}
-  mkdir ${app_path}
-
-  echo -e "${color} Downloading ${component} Artifacts${nocolor}"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-
-
-  echo -e "${color} Unzipping file${nocolor}"
-  cd ${app_path}
-  unzip /tmp/${component}.zip &>>${log_file}
+  app_presetup
 
   echo -e "${color} Download Dependencies${nocolor}"
-  cd ${app_path}
   mvn clean package &>>${log_file}
   mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
 
-  echo -e "${color} Setup SystemD file${nocolor}"
-  cp /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
-  echo -e "${color} Installing MySQL Client${nocolor}"
-  yum install mysql -y &>>${log_file}
-
-  echo -e "${color} Load Schema${nocolor}"
-  mysql -h mysql-dev.devopsb73.shop -uroot -pRoboShop@1 < /schema/${component}.sql &>>${log_file}
-
-  echo -e "${color} Start ${component}${nocolor}"
-  systemctl daemon-reload &>>${log_file}
-  systemctl enable ${component} &>>${log_file}
-  systemctl restart ${component} &>>${log_file
+mysql_schema_setup
+systemD_setup
 }
