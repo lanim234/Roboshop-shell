4e3f9d9c4b7e0b1e9983fabc08720bd2095b95b2
Smith <smith@hdudhhd.com>
2023-05-29 15:46:07 -0500

    commit 9

--- common.sh
+++ common.sh
@@ -8,16 +8,21 @@ app_presetup() {
   echo -e "${color}Adding User${nocolor}"
   useradd roboshop &>>${log_file}
 
+echo $?
+
 rm -rf ${app_path} &>>${log_file}
 mkdir ${app_path}
 
 echo -e "${color} Downloading ${component} Artifacts${nocolor}"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
 
+echo $?
+
 echo -e "${color}Extract Application Content${nocolor}"
 cd ${app_path}
 unzip /tmp/${component}.zip &>>${log_file}
 
+echo $?
 }
 
 
@@ -25,11 +30,14 @@ systemD_setup() {
   echo -e "${color}Set up SystemD Service${nocolor}"
   cp  /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
+echo $?
+
   echo -e "${color}Start ${component} Service${nocolor}"
   systemctl daemon-reload &>>${log_file}
   systemctl enable ${component} &>>${log_file}
   systemctl start ${component} &>>${log_file}
 
+echo $?
 }
 
 
@@ -37,26 +45,42 @@ mysql_schema_setup() {
 echo -e "${color} Installing MySQL Client${nocolor}"
   yum install mysql -y &>>${log_file}
 
+echo $?
+
   echo -e "${color} Load Schema${nocolor}"
   mysql -h mysql-dev.devopsb73.shop -uroot -pRoboShop@1 < /home/centos/Roboshop-shell/schema/${component}.sql &>>${log_file}
 
-
+echo $?
 }
+
+
 nodejs() {
 echo -e "${color}Download Repo${nocolor}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
+echo $?
+
 echo -e "${color}Installing NodeJs${nocolor}"
 yum install nodejs -y &>>${log_file}
 
+echo $?
+
 app_presetup
 
+echo $?
 
 echo -e "${color} Install NodeJS Dependencies${nocolor}"
 npm install &>>${log_file}
 
+echo $?
+
 systemD_setup
+
+echo $?
+
 }
+
+
 
 
 mongo_schema_setup() {
@@ -64,11 +88,17 @@ mongo_schema_setup() {
 echo -e "${color}Copy MongoDB Repo File${nocolor}"
 cp  /home/centos/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
 
+echo $?
+
 echo -e "${color}Install MongodDB Client${nocolor}"
 yum install mongodb-org-shell -y &>>${log_file}
 
+echo $?
+
 echo -e "${color} Load Schema${nocolor}"
 mongo --host mongodb-dev.devopsb73.shop <${app_path}/schema/${component}.js &>>${log_file}
+
+echo $?
 
 }
 
@@ -77,30 +107,53 @@ maven() {
   echo -e "${color} Installing Maven${nocolor}"
   yum install maven -y &>>${log_file}
 
+echo $?
+
   app_presetup
+
+  echo $?
 
   echo -e "${color} Download Dependencies${nocolor}"
   mvn clean package &>>${log_file}
   mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
 
+echo $?
 
 mysql_schema_setup
+
+echo $?
+
 systemD_setup
+
+echo $?
+
 }
+
 
 python_setup() {
   echo -e "${color} Installing Python${nocolor}"
   yum install python36 gcc python3-devel -y &>>${log_file}
 
+  echo $?
+
   app_presetup
+
+echo $?
 
   echo -e "${color} Installing Repos${nocolor}"
   cd ${app_path}
   pip3.6 install -r requirements.txt &>>${log_file}
 
+echo $?
+
 
   echo -e "${color} Setting Up SystemD${nocolor}"
   cp /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
+echo $?
+
 systemD_setup
+
+echo $?
+
 }
