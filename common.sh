0663fc2b6c1de3f0aab20f020bfbe274ca236fa2
Smith <smith@hdudhhd.com>
2023-05-30 20:01:09 -0500

    commit 9

--- common.sh
+++ common.sh
@@ -4,6 +4,16 @@ log_file="/tmp/roboshop.log"
 app_path="/app"
 
 
+status check() {
+if [ $? -eq 0 ]; then
+  echo "SUCCESS"
+else
+  echo "FAILURE"
+fi
+}
+
+
+
 app_presetup() {
   echo -e "${color}Adding User${nocolor}"
   id roboshop &>>${log_file}
@@ -11,11 +21,7 @@ app_presetup() {
     useradd roboshop &>>${log_file}
     fi
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
+status check
 
 rm -rf ${app_path} &>>${log_file}
 mkdir ${app_path}
@@ -23,23 +29,13 @@ mkdir ${app_path}
 echo -e "${color} Downloading ${component} Artifacts${nocolor}"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 echo -e "${color}Extract Application Content${nocolor}"
 unzip /tmp/${component}.zip &>>${log_file}
 cd ${app_path}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 }
 
 
@@ -47,24 +43,14 @@ systemD_setup() {
   echo -e "${color}Set up SystemD Service${nocolor}"
   cp  /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
   echo -e "${color}Start ${component} Service${nocolor}"
   systemctl daemon-reload &>>${log_file}
   systemctl enable ${component} &>>${log_file}
   systemctl start ${component} &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 }
 
 
@@ -74,22 +60,12 @@ mysql_schema_setup() {
 echo -e "${color} Installing MySQL Client${nocolor}"
   yum install mysql -y &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
   echo -e "${color} Load Schema${nocolor}"
   mysql -h mysql-dev.devopsb73.shop -uroot -pRoboShop@1 < /home/centos/Roboshop-shell/schema/${component}.sql &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 }
 
 
@@ -97,48 +73,26 @@ nodejs() {
 echo -e "${color}Download Repo${nocolor}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
+status check
 
 
 echo -e "${color}Installing NodeJs${nocolor}"
 yum install nodejs -y &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 app_presetup
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 echo -e "${color} Install NodeJS Dependencies${nocolor}"
 npm install &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
+status check
 
 systemD_setup
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
+status check
 
 
 }
@@ -151,32 +105,18 @@ mongo_schema_setup() {
 echo -e "${color}Copy MongoDB Repo File${nocolor}"
 cp  /home/centos/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 echo -e "${color}Install MongodDB Client${nocolor}"
 yum install mongodb-org-shell -y &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
+status check
 
 
 echo -e "${color} Load Schema${nocolor}"
 mongo --host mongodb-dev.devopsb73.shop <${app_path}/schema/${component}.js &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 }
 
@@ -185,51 +125,28 @@ fi
   echo -e "${color} Installing Maven${nocolor}"
   yum install maven -y &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
+status check
 
 
   app_presetup
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
   echo -e "${color} Download Dependencies${nocolor}"
   cd ${app_path}
   mvn clean package &>>${log_file}
   mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
+status check
 
 
 mysql_schema_setup
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 systemD_setup
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 }
 
@@ -238,51 +155,26 @@ fi
   echo -e "${color} Installing Python${nocolor}"
   yum install python36 gcc python3-devel -y &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
   app_presetup
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
   echo -e "${color} Installing Repos${nocolor}"
   pip3.6 install -r requirements.txt &>>${log_file}
   cd ${app_path}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 
   echo -e "${color} Setting Up SystemD${nocolor}"
   cp /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 systemD_setup
 
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-fi
-
+status check
 
 }
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,20 +1,26 @@
 source common.sh
-component=shipping
-
+component=rabbitmq
 
 
 
 echo -e "${color} Configuring Erlang Repo${nocolor}"
 curl -s https://packagecloud.io/install/repositories/${component}/erlang/script.rpm.sh | bash &>>${log_file}
+status check
 
 echo -e "${color} Configuring Yum Repos for ${component}${nocolor}"
 curl -s https://packagecloud.io/install/repositories/${component}/${component}-server/script.rpm.sh | bash &>>${log_file}
+status check
+
 
 echo -e "${color} Installing ${component}${nocolor}"
 yum install ${component}-server -y &>>${log_file}
+status check
 
 systemD_setup
+status check
+
 
 echo -e "${color} Create User and permission${nocolor}"
 ${component}ctl add_user roboshop roboshop123 &>>${log_file}
 ${component}ctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log_file}
+status check
