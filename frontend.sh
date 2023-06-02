2d97ab81449dc6d34de72fe9b3b3a4d71da8fc91
Smith <smith@hdudhhd.com>
2023-06-01 19:13:19 -0500

    commit 9

--- frontend.sh
+++ frontend.sh
@@ -12,10 +12,7 @@ app_presetup
 
 status_check
 
-echo -e "${color}Update Frontend Info${nocolor}"
-cp /home/centos/Roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file)
 
-#empty line
 systemD_setup
 
 status_check
