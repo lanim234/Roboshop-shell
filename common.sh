6b2129fcd28f2e8f162490d29d119e0fdb5f75c8
Smith <smith@hdudhhd.com>
2023-06-01 20:13:18 -0500

    commit 9

--- common.sh
+++ common.sh
@@ -134,7 +134,7 @@ status_check
 
   echo -e "${color} Download Dependencies${nocolor}"
   mvn clean package &>>${log_file}
-  mv /home/centos/Roboshop-shell/target/${component}-1.0.jar.original ${component}.jar &>>${log_file}
+  mv /target/${component}-1.0.jar.original ${component}.jar &>>${log_file}
 
 status_check
 
